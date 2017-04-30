e0ff53b4a9db4d9a7a08d9cc8f270f61daa57253
Anthony Castor <Anthony Castor>
2017-04-30 21:46:58 +0800

    adding ejs

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express')
 var app = express()
+var routes = require('./routes');//Incude Routes Folder
 
+app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Here is the location of sample nodejs-web-application!')
-})
+app.locals.pagetitle = "Awesome Website ";
+
+app.get('/', routes.index);
+app.get('/about', routes.about);
+
+app.get('*', function(req, res){
+	res.send('Bad Route');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
